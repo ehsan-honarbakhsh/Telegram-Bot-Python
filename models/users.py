db0be8ccc2f1866a3a23fa43f3fac563710a36e2
ehsan <ehsan.honarbakhsh96@gmail.com>
2025-04-25 08:00:05 +0100

    User Table
    -The models has been added to the directory
    -The user table has been created

---
+++ models/users.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    chat_id = Column(Integer, unique=True, nullable=False, index=True)
+    username = Column(String, nullable=True)
+    first_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<User(chat_id={self.chat_id}, username={self.username})>"
